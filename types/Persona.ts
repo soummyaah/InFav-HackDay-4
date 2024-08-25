4f96cae7f122abaef03e2e0a29710eba2d94b23c
Soumya Sharma <soumya13106@iiitd.ac.in>
2024-08-26 02:11:32 +0530

    Update Persona.ts

--- types/Persona.ts
+++ types/Persona.ts
@@ -1,9 +1,10 @@
 export type Persona = {
     id: string,
     user_id: string,
-    professional_identity: string,
+    company: string,
+    job_title: string,
     focus_area: string,
-    story: string,
+    personal_story: string,
     business_story: string,
     target_audience: Object,
     persona_data: Object
